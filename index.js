a013b10aaeeeb2c052b014cd3ba222c9b1473552
TunerBotDev <92864809+TunerBotDev@users.noreply.github.com>
2022-10-11 18:46:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Cluster = require('discord-hybrid-sharding');
-
+const { token } = require("./settings.json")
 const manager = new Cluster.Manager(`${__dirname}/bot.js`, {
-    totalShards: 'auto', // or 'auto'
+    totalShards: 7, // or 'auto'
     /// Check below for more options
     shardsPerClusters: 3,
     // totalClusters: 7,
     mode: 'process', // you can also choose "worker"
-    token: "MTAyMTY1NjIzNzc0MzU1MDQ4NQ.GfkRTy.lF921MernUOQD6mj5LV8764hF_TE9Ti6Cfygco",
+    token: token,
 });
 
 manager.on('clusterCreate', cluster => console.log(`Launched Cluster ${cluster.id}`));
