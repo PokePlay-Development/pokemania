bcd2beedb9a1cfa3c3e4309fb0ebae70ad97f5de
ironman11111 <93186727+ironman11111@users.noreply.github.com>
2022-12-30 17:18:26 +0530

    Update test.js

--- slashCommands/test.js
+++ slashCommands/test.js
@@ -1,22 +1,9 @@
-const { Client, Collection, Intents, MessageButton, MessageActionRow, MessageAttachment, MessageEmbed } = require("discord.js");
-const { color } = require("../../settings.json").embeds;
-const fetch = require("node-fetch");
-const User = require("../../models/user");
-const Spawn = require("../../models/spawn.js");
-const { instanceToPlain } = require("class-transformer");
-const Pokemon = require("../../classes/pokemon");
-let embed = new Discord.MessageEmbed()
-                        .setTitle(`A wild pokémon has appeared!`)
-                        .setColor(require("./settings.json").embeds.color)
-                        .setImage(`attachment://pokemon.png`)
-                        .setDescription(`Guess the pokémon and type \`${guild.prefix}catch <pokémon>\` to catch it!`)
 module.exports = {
-name: "test",
- run: async (client, message, args) => {
-if (!args[0]) return message.reply({
-                        embeds: [embed],
-                        components: row,
-                        files: [attachment]
-                    })
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
 
-                    
