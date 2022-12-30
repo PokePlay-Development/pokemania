ed8479f6fc5766c98c3c246b8c07fbe02c095052
ironman11111 <93186727+ironman11111@users.noreply.github.com>
2022-12-30 17:18:53 +0530

    Update test.js

--- slashCommands/test.js
+++ slashCommands/test.js
@@ -1,6 +1,6 @@
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
+		.setName('king')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		await interaction.reply('Pong!');
